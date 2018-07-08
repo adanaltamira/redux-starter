7e3deaafb2595c89d3313157e458a8741c147a65
Adan Altamira <adanaltamira@Adans-MacBook.socal.rr.com>
2018-07-07 22:48:03 -0700

    ✨ manage course update to read and write courses

---
+++ src/components/course/CourseForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import TextInput from '../common/TextInput';
+import SelectInput from '../common/SelectInput';
+
+const CourseForm = ({course, allAuthors, onSave, onChange, loading, errors}) => {
+	return (
+		<form>
+			<h1>Manage Course</h1>
+			<TextInput
+				name="title"
+				label="Title"
+				value={course.title}
+				onChange={onChange}
+				error={errors.title}/>
+			
+			<SelectInput
+				name="authorId"
+				label="Author"
+				value={course.authorId}
+				defaultOption="SelectAuthor"
+				options={allAuthors}
+				onChange={onChange}
+				error={errors.authorId}/>
+			<input
+				type="submit"
+				disabled={loading}
+				value={loading ? 'Saving...' : 'Save'}
+				className="btn btn-primary"
+				onClick={onSave}/>
+		</form>
+	);
+};
+
+CourseForm.propTypes = {
+	course: React.PropTypes.object,
+	allAuthors: React.PropTypes.array, 
+	onSave: React.PropTypes.func, 
+	onChange: React.PropTypes.func, 
+	loading: React.PropTypes.bool, 
+	errors: React.PropTypes.object
+};
+
+export default CourseForm;
